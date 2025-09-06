eb529697a4271dc12029b29c746c1c3b696dd1f6
yoshiooooo <yoshikimiyamoto0829@gmail.com>
2025-09-06 13:07:12 +0900

    feat:spring initializerの配置

---
+++ src/test/java/com/example/ecsite/EcsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
