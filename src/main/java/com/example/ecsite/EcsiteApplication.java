8d5042fd76fc72f4f4ed77273cf56f72bc88b78b
yoshiooooo <yoshikimiyamoto0829@gmail.com>
2025-09-11 09:16:02 +0900

    ログイン画面まで表示

--- src/main/java/com/example/ecsite/EcsiteApplication.java
+++ src/main/java/com/example/ecsite/EcsiteApplication.java
@@ -2,12 +2,13 @@ package com.example.ecsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EcsiteApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EcsiteApplication.class, args);
-	}
-
+public class EcsiteApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EcsiteApplication.class);
+    }
 }
--- src/test/java/com/example/ecsite/EcsiteApplicationTests.java
+++ src/test/java/com/example/ecsite/EcsiteApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.ecsite;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class EcsiteApplicationTests {
 
 	@Test
